d4b12cc02ee51fbb98ba59f6a4744d2fba4e0e24
Shifly Inner Beauty <shifly.20063@mhs.unesa.ac.id>
2023-08-28 15:33:03 +0700

    update content

--- pages/register.tsx
+++ pages/register.tsx
@@ -31,7 +31,7 @@ export default function Register() {
                     </div>
                     <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-500 dark:hover:bg-blue-800 dark:focus:ring-blue-500">Register</button>
                     <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        you have an account? <a href="/" className="text-blue-700 hover:underline dark:text-blue-500">Sign In</a>
+                        do you have an account? <a href="/" className="text-blue-700 hover:underline dark:text-blue-500">Sign In</a>
                     </div>
                 </form>
             </div>
